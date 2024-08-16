024ff84614a81d2ca15fc116a482ea62063b6521
Dimitri <philjke@mail.ru>
2024-08-17 01:30:19 +0700

    fix rss feed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,12 +1,14 @@
-import rss, { pagesGlobToRssItems } from "@astrojs/rss";
+import rss from "@astrojs/rss";
+
 import { getCollection } from "astro:content";
 
 export async function GET(context) {
-  const posts = await getCollection("blog");
+  const posts = await getCollection("posts");
   return rss({
     title: "Astro Learner | Blog",
     description: "My journey learning Astro",
     site: context.site,
+
     items: posts.map((post) => ({
       title: post.data.title,
       pubDate: post.data.pubDate,
