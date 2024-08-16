f06cbcf37a362f44042f51d5f85bccea617a1716
Dimitri <philjke@mail.ru>
2024-08-16 23:35:09 +0700

    add react  and preact integrations in islands

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,17 @@
 import { defineConfig } from "astro/config";
+import preact from "@astrojs/preact";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://phil-astro.netlify.app/",
+  integrations: [
+    preact({
+      include: ["**/preact/*"],
+    }),
+    react({
+      include: ["**/react/*"],
+    }),
+  ],
 });
