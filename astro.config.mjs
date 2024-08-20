a6e154dc7cd71666985777186614e9f2a1ee8fe1
phil <philjke@mail.ru>
2024-08-20 11:53:30 +0700

    add netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,20 @@
 import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
-
 import react from "@astrojs/react";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://phil-astro.netlify.app/",
   image: {
-    domains: ["bitcar.rent"],
+    domains: ["bitcar.rent"]
   },
-  integrations: [
-    preact({
-      include: ["**/preact/*"],
-    }),
-    react({
-      include: ["**/react/*"],
-    }),
-  ],
+  integrations: [preact({
+    include: ["**/preact/*"]
+  }), react({
+    include: ["**/react/*"]
+  })],
+  output: "server",
+  adapter: netlify()
 });
