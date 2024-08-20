f43d05c5d792301e72823386abe22975a3980a2e
phil <philjke@mail.ru>
2024-08-20 11:39:57 +0700

    some fixes

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,12 @@
 /// <reference path="../.astro/types.d.ts" />
+/// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly BITCAR_CONSUMER_KEY: string;
+  readonly BITCAR_CONSUMER_SECRET: string;
+  // больше переменных env...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
